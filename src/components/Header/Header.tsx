7697ac1609de324aaa93694b13b4671ac390d1c0
julio189 <julio.c.jesus@hotmail.com>
2023-09-16 11:15:16 -0300

    refactor: rename and reorganize the components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,36 +1,12 @@
+import TopHeader from './TopHeader/TopHeader'
 import './Header.sass'
-import TextTwoColor from './TextTwoColor/TextTwoColor'
-import ShieldCheck from '../icons/ShieldCheck'
-import Truck from '../icons/Truck'
-import CreditCard from '../icons/CreditCard'
+import Logo from './Logo/Logo'
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="header__items">
-        <TextTwoColor
-          icon={<ShieldCheck />}
-          firstText="Compra "
-          secondText="100% segura"
-          firstColor="Gray"
-          secondColor="Pink"
-        />
-        <TextTwoColor
-          icon={<Truck />}
-          firstText="Frete grátis "
-          secondText="acima de R$ 200"
-          firstColor="Pink"
-          secondColor="Gray"
-        />
-        <TextTwoColor
-          icon={<CreditCard />}
-          firstText="Parcele "
-          secondText="suas compras"
-          firstColor="Pink"
-          secondColor="Gray"
-          isLast
-        />
-      </div>
+      <TopHeader />
+      <Logo />
     </header>
   )
 }
